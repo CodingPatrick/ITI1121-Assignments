9cff61bf76fe25add2c86c43efe9690a98cdafd7
Kamil Adjali <59594088+KamilKode@users.noreply.github.com>
2020-03-19 18:29:20 -0400

    Add files via upload

---
+++ a3_300115794_300112374/Utils.java
@@ -0,0 +1,208 @@
+import java.util.Random;
+import java.io.Console;
+import java.lang.Math;
+
+public class Utils {
+	public static final Random generator = new Random();
+	public static final Console console = System.console();
+	public static final String NEW_LINE = System.getProperty("line.separator");
+
+
+    /**
+     * This method rotates array of size lines * columns referenced
+     * by transformedBoard by 90 degree clockwise. For example, the
+     * 3*3 game
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method rotates it into the game
+     *
+     * 7 | 4 | 1
+     * ----------
+     * 8 | 5 | 2
+     * ----------
+     * 9 | 6 | 3
+     *
+     * that is, after the method, transformedBoard = {7,4,1,8,5,2,9,6,3}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+
+    public static void rotate(int lines, int columns, int[] transformedBoard){
+
+        // YOUR CODE HERE
+        int length = lines*columns;
+        int tmpBoard[] = new int[lines*columns]; // initialize a temp board
+        int position=0;
+
+        if (lines != columns){
+            return; //rotation is only possible when lines == columns
+        }
+
+        for (int i=0; i< lines; i++){
+            for (int j=1; j <= columns; j++){
+                tmpBoard[position] = transformedBoard[length - ((j*lines)-i)]; // finds all the corresponding suqares for the rotation and adds them to a temp array
+                position++;
+            }
+        }
+
+        for (int i=0; i<lines*columns; i++){
+            transformedBoard[i] = tmpBoard[i]; // place the rotated values into the original board
+        }
+    
+    }
+
+    /**
+     * This method does an horizontal symmetry on array of size lines * columns referenced
+     * by transformedBoard. For example, the
+     * 3*3 game
+     *
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method transforms it into the game   
+     *
+     * 7 | 8 | 9
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 1 | 2 | 3
+     *
+     * that is, after the method, transformedBoard = {7,8,9,4,5,6,1,2,3}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+    public static  void horizontalFlip(int lines, int columns, int[] transformedBoard){
+
+        // YOUR CODE HERE
+        int length = lines*columns;
+        int tmp;
+        int position=0;
+
+        for (int i=1; i <= Math.floorDiv(lines, 2); i++){
+            for(int j=0; j < columns; j++){
+                tmp = transformedBoard[position]; // stores the value at the position
+                transformedBoard[position] = transformedBoard[length-((i*columns)-j)]; // replaces the value at position with the one on the other side of the reflexion
+                transformedBoard[length-((i*columns)-j)] = tmp; // puts the inital value of position in the other side of the reflection
+                if (!(position < Math.floorDiv(length, 2))){    // checks if we're still in the first half of the board
+                    break;
+                }
+                position++;
+            }
+        }
+    }
+
+   /**
+     * This method does an vertical symmetry on array of size lines * columns referenced
+     * by transformedBoard. For example, the
+     * 3*3 game
+     *
+     * 1 | 2 | 3
+     * ----------
+     * 4 | 5 | 6
+     * ----------
+     * 7 | 8 | 9
+     *
+     * for which lines = 3 and columns = 3 is represented by the array
+     * transformedBoard = {1,2,3,4,5,6,7,8,9}
+     *
+     * the method transforms it into the game   
+     *
+     * 3 | 2 | 1
+     * ----------
+     * 6 | 5 | 4
+     * ----------
+     * 9 | 8 | 7
+     *
+     * that is, after the method, transformedBoard = {3,2,1,6,5,4,9,8,7}
+     *
+     * @param lines
+     *  the number of lines in the game
+     * @param columns
+     *  the number of columns in the game
+     * @param transformedBoard
+     *  Reference to a 1 dimentional array of size columns x lines
+     */
+
+    public static  void verticalFlip(int lines, int columns, int[] transformedBoard){
+ 
+        // YOUR CODE HERE
+        int length = lines*columns;
+        int tmp;
+        int position=0;
+
+        for (int i=1; i <= lines; i++){
+            for(int j=1; j < Math.floorDiv(columns, 2)+1; j++){
+                tmp = transformedBoard[position]; // stores the value at the position
+                transformedBoard[position] = transformedBoard[(i*columns)-j]; // replaces the value at position with the one on the other side of the reflexion
+                transformedBoard[(i*columns)-j] = tmp; // puts the inital value of position in the other side of the reflection
+                position++;
+            }
+            position += (columns - Math.floorDiv(columns, 2));
+        }
+
+    }
+
+    private static void test(int lines, int columns){
+    	int[] test;
+    	test = new int[lines*columns];
+    	for(int i = 0 ; i < test.length; i++){
+    		test[i] = i;
+    	}
+    	System.out.println("testing " + lines + " lines and " + columns + " columns.");
+    	System.out.println(java.util.Arrays.toString(test));
+    	horizontalFlip(lines,columns,test);
+    	System.out.println("HF => " + java.util.Arrays.toString(test));
+    	horizontalFlip(lines,columns,test);
+    	System.out.println("HF => " + java.util.Arrays.toString(test));
+    	verticalFlip(lines,columns,test);
+    	System.out.println("VF => " + java.util.Arrays.toString(test));
+    	verticalFlip(lines,columns,test);
+    	System.out.println("VF => " + java.util.Arrays.toString(test));
+    	if(lines == columns){
+    		for(int i = 0; i < 4; i++) {
+		    	rotate(lines,columns,test);
+		    	System.out.println("ROT => " + java.util.Arrays.toString(test));    			
+    		}
+    	}
+    }
+
+    public static void main(String[] args){
+    	int[] test;
+    	int lines, columns;
+
+    	test(2,2);
+    	test(2,3);
+    	test(3,3);
+    	test(4,3);
+    	test(4,4);
+
+
+    }
+}
